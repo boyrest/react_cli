fe48342c4fd9a6c03b74f0fa5860a3873eadbc04
Baron Zhu <425728299@qq.com>
2017-09-13 18:37:30 +0800

    update code

--- src/containers/me/subContainer/MeInfo/index.jsx
+++ src/containers/me/subContainer/MeInfo/index.jsx
@@ -17,10 +17,10 @@ export default class MeInfo extends Component {
     render(){
         return(
             <div id="me-info-container" className={this.props.show?'':'hidden-element'}>
-               <div class="m-myoder">
-                    <div class="lt">我的订单</div>
-                    <div class="icon_link order_list">查看全部订单 <img src={chevronRight}/></div>
-                    <ul class="order_state">
+               <div className="m-myoder">
+                    <div className="lt">我的订单</div>
+                    <div className="icon_link order_list">查看全部订单 <img src={chevronRight}/></div>
+                    <ul className="order_state">
                         <li id="unpayed" class="box1"><img src={iconWallet}/><p>待付款</p></li>
                         <li id="pending" class="box2"><img src={iconScan}/><p>待确认</p></li>
                         <li id="confirmed" class="box3"><img src={iconBox}/><p>待发货</p></li>
@@ -29,12 +29,12 @@ export default class MeInfo extends Component {
                         <li id="canceled" class="box6"><img src={iconMoney}/><p>商家取消</p></li>
                     </ul>
                 </div>
-                <ul id="m-link">
-                    <li class="u-bar addr_link">地址簿<div class="icon_link"><img src={chevronRight}/></div></li>
-                    <li class="u-bar help_link">帮助<div class="icon_link"><img src={chevronRight}/></div></li>
-                    <li class="u-bar us_link">联系我们<div class="icon_link"><img src={chevronRight}/></div></li>
-                    <li class="u-bar">应用版本号<div class="bar_version" id="appVersion"></div></li>
-                    <li class="u-bar">资源版本号<div class="bar_version" id="sourceCodeVersion"></div></li>
+                <ul className="m-link">
+                    <li className="u-bar">地址簿<div class="icon_link"><img src={chevronRight}/></div></li>
+                    <li className="u-bar">帮助<div class="icon_link"><img src={chevronRight}/></div></li>
+                    <li className="u-bar">联系我们<div class="icon_link"><img src={chevronRight}/></div></li>
+                    <li className="u-bar">应用版本号<div class="bar_version" id="appVersion"></div></li>
+                    <li className="u-bar">资源版本号<div class="bar_version" id="sourceCodeVersion"></div></li>
                 </ul>
             </div>
         )
